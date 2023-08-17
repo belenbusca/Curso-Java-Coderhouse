ca9180413c6ae844daf27094ed6e62bd71e70ea3
belenbusca <clari_belu@live.com>
2023-08-17 11:53:20 -0300

    ejercicio numero flotante

--- manejoDatosTipoFloat/NumeroFlotanteBusca.java
+++ manejoDatosTipoFloat/NumeroFlotanteBusca.java
@@ -2,6 +2,11 @@ package manejoDatosTipoFloat;
 
 public class NumeroFlotanteBusca {
     public static void main(String[] args) {
-    
+        float numero = 12.7f;
+        int parteEntera = (int) numero;
+        float parteDecimal = numero - parteEntera;
+        System.out.println("numero: " + numero);
+        System.out.println("parte entera: " + parteEntera);
+        System.out.println("parte decimal: " + parteDecimal);
     }
 }
