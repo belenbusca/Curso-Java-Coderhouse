664af2241d332f4af5f66280cd384c0da1358de8
belenbusca <clari_belu@live.com>
2023-08-30 17:40:42 -0300

    clase 3

---
+++ clases/clase3/arrayEjemplo.java
@@ -0,0 +1,12 @@
+package clases.clase3;
+
+public class arrayEjemplo{
+    public static void main(String[] args) {
+        int[] edades = new int[5]; 
+        // NOTA: se puede defini tmb como int edades[] como haciamos en la facu, pero dice que no es convencional
+        String[] nombresVarones;
+        String[] nombresMujeres;
+
+        
+    }
+}
