8b591abffd08fca8ebe7e1a22c57c5a92ea9d137
belenbusca <clari_belu@live.com>
2023-09-11 21:54:59 -0300

    Clase 5

---
+++ clases/clase5/ejemplos/Preuba.java
@@ -0,0 +1,15 @@
+package clases.clase5.ejemplos;
+
+public class Preuba {
+    public static void main(String[] args) {
+        Perro firu = new Perro();
+        firu.setEdad(2);
+    
+        Gato mishi = new Gato();
+    
+        mishi.lavarse();
+    
+        // Animal michu = new Gato(); // Polimorfismo
+        // michu = new Perro();
+      }
+}
