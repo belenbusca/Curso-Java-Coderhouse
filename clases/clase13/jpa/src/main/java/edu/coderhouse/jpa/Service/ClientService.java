0d6747b3a747032af81fe264a4ee7295347d3bc2
belenbusca <clari_belu@live.com>
2023-10-28 15:28:07 -0300

    clase 13 rest externo

---
+++ clases/clase13/jpa/src/main/java/edu/coderhouse/jpa/Service/ClientService.java
@@ -0,0 +1,30 @@
+package edu.coderhouse.jpa.Service;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.coderhouse.jpa.api.ClientRestApi;
+import edu.coderhouse.jpa.Entity.ClienteEntity;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRestApi clientRestApi;
+
+    public List<String> getClients() {
+        return clientRestApi.getClients();
+    }
+
+    public String getClientById(String id) {
+        return clientRestApi.getClientById(id);
+    }
+
+    public String saveClient(ClienteEntity client) {
+        return clientRestApi.saveClient(client);
+    }
+
+}
